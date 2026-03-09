178494e60aaada0630c7e5a05c7118a33ca4ce1f
Mavis Ou <mmmavis@users.noreply.github.com>
2026-03-09 14:29:13 -0700

    feat(accelevent): add CSS build pipeline with SCSS and PostCSS

---
+++ accelevent/postcss.config.js
@@ -0,0 +1,14 @@
+import autoprefixer from "autoprefixer";
+
+const isDev = process.env.NODE_ENV !== "production";
+
+export default {
+  plugins: [autoprefixer],
+  map: isDev
+    ? {
+        inline: false,
+        annotation: true,
+        sourcesContent: true,
+      }
+    : false,
+};
